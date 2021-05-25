7ab9bef9063ef06e5c7127ce03c6379922452e19
Rostyslav <martynykros@gmail.com>
2021-05-25 14:18:44 +0300

    added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.page params[:page]
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  paginates_per 2
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
